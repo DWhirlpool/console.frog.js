d350e517707e59a5e809dfd1d5b12cfe274b778b
Daniel Lu <95860724+DWhirlpool@users.noreply.github.com>
2022-03-30 10:57:18 +0800

    Create console.frog.js

---
+++ console.frog.js
@@ -0,0 +1,69 @@
+/**
+ *  Console.frog. A better way to log!
+ *    - Logs a frog, saying your log... Yep!
+ *
+ *  MIT licensed
+ *  Copyright (C) 2022 DWhirlpool, https://dwhirlpool.github.io
+ */
+
+(function dothething() {
+  // Act on either the window.console, or the normal console.
+  var con = console;
+  if (typeof(window) !== 'undefined') {
+    con = window.console;
+  }
+
+  con.frog = function() {
+
+    var i,
+      css = "color: green";
+
+    // Should be enough to make this fine in node.
+    var inBrowser = (typeof window !== 'undefined');
+    
+    // This looks like a frog, right?
+    // Taken from here - http://chris.com/ascii/index.php?art=animals/frogs
+
+    var frog;
+    if( inBrowser ) {
+      frog = ['%c%c',
+        "%c         _,-.  %c",
+        "%c ,-. ,--'  o ) %c",
+        "%c \\(,' '  ,,-' %c",
+        "%c,-.\\-.__,\\\\_%c",
+        "%c\\(`--'    `\\ %c",
+        '%c%c'];
+    } else {
+      frog = ['',
+        "         _,-.  ",
+        " ,-. ,--'  o ) ",
+        " \\(,' '  ,,-' ",
+        ",-.\\-.__,\\\\_",
+        "\\(`--'    `\\ ",
+        ''];
+    }
+      
+
+    // Gets args as a string
+    var args = Array.prototype.slice.call(arguments);
+    var stringOfArgs = args.join(' ');
+
+    // Add the bubble if there is something to log!
+    if( stringOfArgs.length > 0 ) {  
+      frog[1] = frog[1] + "   ---" + ("-".repeat(stringOfArgs.length)) + "-";
+      frog[2] = frog[2] + "-(   " + stringOfArgs + "   )";
+      frog[3] = frog[3] + "    ---" + ("-".repeat(stringOfArgs.length)) + "-";
+    }
+
+    // Log the frog!
+    if( inBrowser ) {
+      for( i = 0; i < frog.length; i++ ) {
+        console.log(frog[i], css, "");
+      }
+    } else {
+      for( i = 0; i < frog.length; i++ ) {
+        console.log(frog[i]);
+      }  
+    }
+  }
+})();
